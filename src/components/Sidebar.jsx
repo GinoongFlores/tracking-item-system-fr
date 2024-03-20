b251ceec92571993843261693dc6127b9236641b
ginoongflores <florespaul161@gmail.com>
2024-03-20 09:33:21 +0800

    Revert "temp(Sidebar): layout"
    This reverts commit b4d2af81c00f52f889496a1357eb352a547a49f4.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,8 @@
 import { Link } from "react-router-dom";
-import { createContext, useContext, useState } from "react";
+import { useState } from "react";
 import { FaBackward } from "react-icons/fa";
-import { FaForward } from "react-icons/fa";
-import { BsThreeDotsVertical } from "react-icons/bs";
 
-const SidebarContext = createContext();
-export default function Sidebar({ children }) {
+const Sidebar = () => {
   const sidebarLinks = [
     {
       name: "Home",
@@ -44,8 +41,6 @@ export default function Sidebar({ children }) {
   const [isMobile, setIsMobile] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
-  const [expanded, setExpanded] = useState(false);
-
   const toggleMobile = () => {
     setIsMobile(!isMobile);
     console.log(isMobile);
@@ -58,7 +53,7 @@ export default function Sidebar({ children }) {
 
   return (
     <>
-      {/* <button
+      <button
         // data-drawer-target="sidebar-multi-level-sidebar"
         // data-drawer-toggle="sidebar-multi-level-sidebar"
         // aria-controls="sidebar-multi-level-sidebar"
@@ -80,91 +75,71 @@ export default function Sidebar({ children }) {
             d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
           />
         </svg>
-      </button> */}
+      </button>
 
-      <aside className="h-screen w-44">
-        <nav className="h-full flex flex-col bg-white border-r shadow-sm">
-          <div className="p-4 pb-2 flex justify-between items-center">
-            <img
-              src="https://img.logoipsum.com/243.svg"
-              alt=""
-              className={`overflow-hidden transition-all ${
-                expanded ? "w-32" : "w-0"
-              }`}
-            />
+      <div className="hidden md:visible">
+        <aside
+          id="sidebar-multi-level-sidebar"
+          className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform`}
+          aria-label="Sidebar"
+        >
+          <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+            {/* Sidebar Links */}
 
-            <button
-              className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100"
-              onClick={() => setExpanded((prevState) => !prevState)}
-            >
-              {expanded ? <FaForward /> : <FaBackward />}
-            </button>
+            <ul className="space-y-2 font-medium">
+              {sidebarLinks.map((link, index) => (
+                <li key={index}>
+                  <Link
+                    to={link.link}
+                    className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                  >
+                    {link.icon}
+                    <span className="ms-3">{link.name}</span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </div>
+        </aside>
+      </div>
 
-          <SidebarContext.Provider value={{ expanded }}>
-            <ul className="flex-1 px-3">{children}</ul>
-          </SidebarContext.Provider>
+      {/* mobile  */}
 
-          <div className="border-t flex px-3">
-            <img
-              src="https://ui-avatars.com/api/?background=c7d2fe&color=3730a3&bold=true"
-              alt=""
-              className="w-10 h-10 rounded-md"
-            />
-
-            <div
-              className={`flex justify-between items-center overflow-hidden transition-all ${
-                expanded ? "w-32 ml-3" : "w-0"
-              }`}
-            >
-              <div className="leading-4">
-                <h4 className="font-semibold">Joe</h4>
-                <span className="text-xs text-gray-600">joe@gmail.com</span>
-              </div>
-              <BsThreeDotsVertical size={20} />
-            </div>
+      <aside
+        id="sidebar-multi-level-sidebar"
+        className={`h-screen transition-transform ${
+          isOpen
+            ? "fixed top-0 left-0 z-40 w-64 "
+            : "fixed transition-transform left-[-100%]"
+        }
+               `}
+        aria-label="Sidebar"
+      >
+        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+          {/* Sidebar Links */}
+          <div
+            className="flex justify-self-start cursor-pointer"
+            onClick={toggleOpen}
+          >
+            <FaBackward className="dark:text-gray-400 " />
           </div>
-        </nav>
+          <ul className="space-y-2 font-medium">
+            {sidebarLinks.map((link, index) => (
+              <li key={index}>
+                <Link
+                  to={link.link}
+                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                >
+                  {link.icon}
+                  <span className="ms-3">{link.name}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </aside>
     </>
   );
-}
+};
 
-export function SideBarItem({ icon, text, active, alert }) {
-  const { expanded } = useContext(SidebarContext);
-  return (
-    <li
-      className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer
-      transition-colors group ${
-        active
-          ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
-          : "hover:bg-indigo-50 text-gray-600"
-      }`}
-    >
-      {icon}
-      <span
-        className={`overflow-hidden transition-all w-52 ml-3 
-      ${expanded ? "w-52 ml-3" : "w-0"}`}
-      >
-        {text}
-      </span>
-      {alert && (
-        <div
-          className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${
-            expanded ? " " : "top-2"
-          }`}
-        />
-      )}
-
-      {!expanded && (
-        <div
-          className={`
-      absolute left-full rounded-md px-2 py-1 ml-6 bg-indigo-100 text-indigo-800 text-sm invisible opacity-20
-      -translate-x-3 transition-all group-hover:opacity-100 group-hover:translate-x-0`}
-        >
-          {text}
-        </div>
-      )}
-    </li>
-  );
-}
+export default Sidebar;
