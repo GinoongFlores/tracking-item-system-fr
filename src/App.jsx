2de5212e889ef3c99edc4687c68968bde6296620
ginoongflores <florespaul161@gmail.com>
2024-03-20 14:43:49 +0800

    temp: router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Home from "./pages/admin/Home";
 import Items from "./pages/admin/Items";
+import Users from "./pages/admin/Users";
+import Admin from "./pages/admin/Admin";
 
 import Login from "./components/Login";
 import Register from "./components/Register";
 import SuperAdminLayout from "./layouts/SuperAdminLayout";
+import { AuthProvider } from "./context/AuthContext";
+
+import { useNavigate, useLocation } from "react-router-dom";
 
 import {
   BrowserRouter as Router,
@@ -18,8 +23,10 @@ const routerSuperAdmin = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<SuperAdminLayout />}>
-        <Route path="home" element={<Home />} />
+        <Route path="home" index element={<Home />} />
         <Route path="items" element={<Items />} />
+        <Route path="users" element={<Users />} />
+        <Route path="admin" element={<Admin />} />
       </Route>
 
       <Route path="login" element={<Login />} />
