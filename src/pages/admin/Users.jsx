d1e43f4f5507a4bd7951416392999a8ac37df818
ginoongflores <florespaul161@gmail.com>
2024-03-29 15:49:31 +0800

    ref(Users): temp add pagination

--- src/pages/admin/Users.jsx
+++ src/pages/admin/Users.jsx
@@ -1,4 +1,3 @@
-import AxiosInstance from "../../api/Axios";
 import { useEffect, useState } from "react";
 import { UsersCards } from "../../components/UsersCards";
 import { useUserContext } from "../../context/UsersContext";
@@ -73,10 +72,97 @@ export const SearchBar = ({ onSearch }) => {
   );
 };
 
+export const Paginate = ({ totalPages, currentPages, onPageChanges }) => {
+  const pages = [...Array(totalPages).keys()].map((i) => i + 1);
+
+  return (
+    <>
+      <nav aria-label="Page navigation example">
+        <ul className="flex items-center -space-x-px h-8 text-base">
+          <li>
+            <button
+              href="#"
+              onClick={() => onPageChanges(currentPages - 1)}
+              disabled={currentPages === 1}
+              className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              <span className="sr-only">Previous</span>
+              <svg
+                className="w-2.5 h-2.5 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M5 1 1 5l4 4"
+                />
+              </svg>
+            </button>
+          </li>
+          {pages.map((page) => (
+            <li key={page}>
+              <button
+                onClick={() => onPageChanges(page)}
+                // className={page === currentPages ? "current" : ""}
+                className={`px-4 py-2 border rounded ${
+                  page === currentPages
+                    ? "z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+                    : "flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                }`}
+              >
+                {page}
+              </button>
+            </li>
+          ))}
+
+          <li>
+            <button
+              onClick={() => onPageChanges(currentPages + 1)}
+              disabled={currentPages === totalPages}
+              className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              <span className="sr-only">Next</span>
+              <svg
+                className="w-2.5 h-2.5 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
 export const Users = () => {
-  const { fetchUsers, users, toggleActivation, setUsers } = useUserContext();
+  const { fetchUsers, users, toggleActivation, setUsers, totalPages } =
+    useUserContext();
   const [search, setSearch] = useState("");
-  const inactiveUsers = users.filter((user) => user.is_activated === 0);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetchUsers(currentPage);
+    };
+
+    fetchData();
+  }, [fetchUsers, currentPage]);
 
   const handleSearch = (searchInput) => {
     setSearch(searchInput);
@@ -129,6 +215,13 @@ export const Users = () => {
             name={`${user.first_name} ${user.last_name}`}
           />
         ))}
+      </div>
+      <div className="py-4 flex flex-col items-center">
+        <Paginate
+          currentPages={currentPage}
+          totalPages={totalPages}
+          onPageChanges={setCurrentPage}
+        />
       </div>
     </>
   );
