7161a97bd926f30198f7a8d222e14863151258d5
ginoongflores <florespaul161@gmail.com>
2024-04-01 13:37:34 +0800

    feat(StoreAuth): activation, fetching users & handling search

--- src/store/StoreUser.jsx
+++ src/store/StoreUser.jsx
@@ -1,17 +1,25 @@
 import { create } from "zustand";
 import AxiosInstance from "../api/Axios";
 import { toast } from "react-hot-toast";
+import { UserToken } from "../hooks/UserToken";
 
 export const useUser = create((set, get) => ({
   users: [],
-  fetchUsers: async (token) => {
+  totalPages: 0,
+  search: "",
+  currentPage: 1,
+
+  fetchUsers: async (page = 1) => {
+    const token = UserToken();
+    // console.log("fetchUser ", token);
     try {
-      const response = await AxiosInstance.get("/user/list", {
+      const response = await AxiosInstance.get(`/user/list?page=${page}`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
-      set({ users: response.data.data });
+      set({ users: response.data.data, totalPages: response.data.last_page });
+      return response;
     } catch (error) {
       console.log(error);
       const errorMessage = error.response.data.message.error;
@@ -21,5 +29,60 @@ export const useUser = create((set, get) => ({
           : errorMessage
       );
     }
+  },
+
+  toggleActivation: async (userId) => {
+    const token = UserToken();
+    try {
+      const response = await AxiosInstance.post(
+        `/user/${userId}/activation`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const activation = response.data.data.is_activated;
+      activation
+        ? toast.success("User activated")
+        : toast.error("User deactivated");
+      return response;
+    } catch (error) {
+      console.log(error.response);
+    }
+  },
+
+  handleToggleActivation: async (userId) => {
+    const response = await get().toggleActivation(userId);
+
+    if (response.status === 200) {
+      set({
+        users: get().users.map((user) =>
+          user.id === userId
+            ? {
+                ...user,
+                is_activated: +!user.is_activated,
+              }
+            : user
+        ),
+      });
+    }
+  },
+
+  handleSearch: (search) => {
+    set({ search });
+  },
+
+  setCurrentPage: (page) => {
+    set({ currentPage: page });
+  },
+
+  filterUsers: (search) => {
+    return get().users.filter(
+      (user) =>
+        user.first_name.toLowerCase().includes(search.toLowerCase()) ||
+        user.last_name.toLowerCase().includes(search.toLowerCase())
+    );
   },
 }));
