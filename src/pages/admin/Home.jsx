34f2229b1be8e4d5afe93750a1addb9cd1ef6b38
ginoongflores <florespaul161@gmail.com>
2024-04-09 11:52:34 +0800

    feat: init admin pages

--- src/pages/admin/Home.jsx
+++ src/pages/admin/Home.jsx
@@ -1,3 +1,23 @@
+import CountCard from "../../components/CountCard";
+
 export const AdminHome = () => {
-  return <div> Home Admin </div>;
+  const cards = [
+    {
+      count: 1,
+      title: "Users",
+    },
+    {
+      count: 2,
+      title: "Items",
+    },
+    {
+      count: 3,
+      title: "",
+    },
+  ];
+  return (
+    <>
+      <div>Hello World</div>
+    </>
+  );
 };
