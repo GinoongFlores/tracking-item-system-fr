17ec296ac0db17f786540ba2a72616abd554c667
ginoongflores <florespaul161@gmail.com>
2024-04-09 11:55:47 +0800

    ref: modify the use of `UseAuthRedirect & changes on routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
 import { Toaster } from "react-hot-toast";
-import { Routes, Route, RouterProvider } from "react-router-dom";
-import { useAuthRedirect } from "./hooks/UseAuthRedirect";
+import { Routes, Route, RouterProvider, BrowserRouter } from "react-router-dom";
+import { UseAuthRedirect } from "./hooks/UseAuthRedirect";
 
-// components
-import { SuperAdminLayout, CompanyLayout } from "./layouts";
-import { Admin, Users, Home, Items } from "./pages/super_admin";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import {
-  AddCompanyPage,
-  ViewCompanyPage,
-  EditCompanyPage,
-} from "./pages/company";
 import { AuthWrapper } from "./utils/AuthWrapper";
-import { AppRouter } from "./router";
+// import { AppRouter } from "./router";
+import { Router } from "./router";
 
 function App() {
-  useAuthRedirect();
+  UseAuthRedirect();
   return (
     <>
       <div>
         <Toaster position="bottom-center" reverseOrder={false} />
-        <AppRouter />
+        <AuthWrapper>
+          <Router />
+        </AuthWrapper>
       </div>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 /*
  ! Notes:
@@ -12,6 +13,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
