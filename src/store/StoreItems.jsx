e5b45be44ee377e0e3c48a85e9a0dc54e253d51c
ginoongflores <florespaul161@gmail.com>
2024-04-11 14:28:38 +0800

    ref(StoreItems): remove token, add fetch of items & restore

--- src/store/StoreItems.jsx
+++ src/store/StoreItems.jsx
@@ -1,26 +1,17 @@
 import { create } from "zustand";
 import { toast } from "react-hot-toast";
-import { UserToken } from "../hooks/UserToken";
 import AxiosInstance from "../api/Axios";
 
 export const useItems = create((set, get) => ({
   itemData: [],
+  itemTrashedData: [],
   isEmptyItem: false,
 
   addItem: async (values) => {
-    const token = UserToken();
     try {
-      const response = await AxiosInstance.post(
-        "item/add",
-        {
-          ...values,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+      const response = await AxiosInstance.post("item/add", {
+        ...values,
+      });
       set({ itemData: [...get().itemData, response.data.data] });
       toast.success("Item added successfully");
     } catch (error) {
@@ -32,13 +23,8 @@ export const useItems = create((set, get) => ({
   },
 
   fetchUserItem: async () => {
-    const token = UserToken();
     try {
-      const response = await AxiosInstance.get(`item/user`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const response = await AxiosInstance.get(`item/user`);
       set({ itemData: response.data.data, isEmptyItem: false });
     } catch (error) {
       if (error.response.status) {
@@ -50,14 +36,35 @@ export const useItems = create((set, get) => ({
     }
   },
 
-  deleteUserItem: async (itemId) => {
-    const token = UserToken();
+  fetchTrashedItem: async () => {
     try {
-      const response = await AxiosInstance.delete(`item/${itemId}/delete`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
+      const response = await AxiosInstance.get(`item/user/trashed`);
+      set({ itemTrashedData: response.data.data, isEmptyItem: false });
+    } catch (error) {
+      console.log(error.response);
+      set({ isEmpty: true });
+    }
+  },
+
+  restoreUserTrashedItem: async (itemId) => {
+    try {
+      const response = await AxiosInstance.post(`item/${itemId}/user/restore`);
+      set({
+        itemTrashedData: get().itemTrashedData.filter(
+          (item) => item.id !== itemId
+        ),
+        itemData: [...get().itemData, response.data.data],
       });
+      toast.success("Item restored successfully");
+      return response;
+    } catch (error) {
+      console.log(error.response);
+    }
+  },
+
+  deleteUserItem: async (itemId) => {
+    try {
+      const response = await AxiosInstance.delete(`item/${itemId}/delete`);
       set({ itemData: get().itemData.filter((item) => item.id !== itemId) });
       toast.success("Item deleted successfully");
       return response;
