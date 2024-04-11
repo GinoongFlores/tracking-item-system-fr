2f39dcc0a8411c5d8e69bcf93fd59c890581524a
ginoongflores <florespaul161@gmail.com>
2024-04-11 14:27:57 +0800

    ref(Axios): add `axios` interceptors to ensure up to date token

--- src/api/Axios.jsx
+++ src/api/Axios.jsx
@@ -9,4 +9,12 @@ const AxiosInstance = axios.create({
   },
 });
 
+AxiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default AxiosInstance;
