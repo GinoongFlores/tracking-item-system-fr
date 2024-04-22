b04ddd11511654b4241fdc1210c73bdc43a3867d
ginoongflores <florespaul161@gmail.com>
2024-04-22 11:06:31 +0800

    ref(useAuthRedirect): get `userStatus` to redirect not activated users to `/login`

--- src/hooks/useAuthRedirect.jsx
+++ src/hooks/useAuthRedirect.jsx
@@ -10,6 +10,7 @@ export const useAuthRedirect = () => {
   const token = UserToken();
   const loading = useAuth((state) => state.loading);
   const getUser = useAuth((state) => state.getUser);
+  const userStatus = useAuth((state) => state.userStatus);
   const location = useLocation();
   const navigate = useNavigate();
 
@@ -22,8 +23,10 @@ export const useAuthRedirect = () => {
   useEffect(() => {
     if (!token && location.pathname !== "/register") {
       navigate("/login");
+    } else if (!userStatus && location.pathname !== "/register") {
+      navigate("/login");
     } else if (token && location.pathname === "/login") {
       navigate("/");
     }
-  }, [location.pathname, navigate, loading, token]);
+  }, [location.pathname, navigate, loading, token, userStatus]);
 };
