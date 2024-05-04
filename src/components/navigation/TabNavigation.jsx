26fa947be4624e5ca64a78cac5f73afaf4d00fb5
Christian Paul Flores <florespaul161@gmail.com>
2024-05-04 18:23:45 +0800

    feat(TabNavigation): finalize tab reusable component

--- src/components/navigation/TabNavigation.jsx
+++ src/components/navigation/TabNavigation.jsx
@@ -1,168 +1,72 @@
-import { useRef, useMemo } from "react";
-import { useEffect } from "react";
-import { Tabs } from "flowbite";
+import { useState } from "react";
+import classNames from "classnames";
 
-export const TabNavigation = () => {
-  const tabsRef = useRef(null);
+export const TabNavigation = ({ tabs = [] }) => {
+  const [activeTab, setActiveTab] = useState(
+    tabs.length > 0 ? tabs[0].target : ""
+  );
 
-  useEffect(() => {
-    if (tabsRef.current) {
-      const tabs = new Tabs(tabsRef.current);
-
-      tabsRef.current.addEventListener("shown.bs.tab", (event) => {
-        const targetId = event.target.getAttribute("data-tabs-target");
-        const targetElement = document.querySelector(targetId);
-
-        if (targetElement) {
-          targetElement.classList.remove("hidden");
-          targetElement.classList.add("block");
-        }
-      });
-
-      tabsRef.current.addEventListener("hidden.bs.tab", (event) => {
-        const targetId = event.target.getAttribute("data-tabs-target");
-        const targetElement = document.querySelector(targetId);
-
-        if (targetElement) {
-          targetElement.classList.remove("block");
-          targetElement.classList.add("hidden");
-        }
-      });
-    }
-  }, []);
+  const handleTabClick = (event) => {
+    setActiveTab(event.target.getAttribute("data-tabs-target"));
+  };
 
   return (
     <>
-      <div
-        className="mb-4 border-b border-gray-200 dark:border-gray-700"
-        ref={tabsRef}
-      >
+      <div className="mb-4 border-b border-gray-200 dark:border-gray-700 ">
         <ul
           className="flex flex-wrap -mb-px text-sm font-medium text-center"
           id="default-tab"
           data-tabs-toggle="#default-tab-content"
           role="tablist"
-          ref={tabsRef}
         >
-          <li className="me-2" role="presentation">
-            <button
-              className="inline-block p-4 border-b-2 rounded-t-lg"
-              id="profile-tab"
-              data-tabs-target="#profile"
-              type="button"
-              role="tab"
-              aria-controls="profile"
-              aria-selected="false"
-            >
-              Profile
-            </button>
-          </li>
-          <li className="me-2" role="presentation">
-            <button
-              className="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-              id="dashboard-tab"
-              data-tabs-target="#dashboard"
-              type="button"
-              role="tab"
-              aria-controls="dashboard"
-              aria-selected="false"
-            >
-              Dashboard
-            </button>
-          </li>
-          <li className="me-2" role="presentation">
-            <button
-              className="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-              id="settings-tab"
-              data-tabs-target="#settings"
-              type="button"
-              role="tab"
-              aria-controls="settings"
-              aria-selected="false"
-            >
-              Settings
-            </button>
-          </li>
-          <li role="presentation">
-            <button
-              className="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-              id="contacts-tab"
-              data-tabs-target="#contacts"
-              type="button"
-              role="tab"
-              aria-controls="contacts"
-              aria-selected="false"
-            >
-              Contacts
-            </button>
-          </li>
+          {tabs.map((tab) => {
+            const isActive = activeTab === tab.target;
+            const buttonClasses = classNames(
+              "inline-block p-4 border-b-2 rounded-t-lg",
+              {
+                "text-black dark:text-tabText hover:text-TabText dark:hover:text-tabText border-tabText dark:border-tabText":
+                  isActive,
+                "dark:border-transparent text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300":
+                  !isActive,
+              }
+            );
+            return (
+              <li
+                className="me-2 flex-grow"
+                role="presentation"
+                key={tab.target}
+              >
+                <button
+                  onClick={handleTabClick}
+                  className={buttonClasses}
+                  data-tabs-target={tab.target}
+                  type="button"
+                  role="tab"
+                  aria-controls={tab.target.slice(1)}
+                  aria-selected={activeTab === tab.target}
+                >
+                  {tab.title}
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
-      <div id="default-tab-content" ref={tabsRef}>
-        <div
-          className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-          id="profile"
-          role="tabpanel"
-          aria-labelledby="profile-tab"
-        >
-          <p className="text-sm text-gray-500 dark:text-gray-400">
-            This is some placeholder content the{" "}
-            <strong className="font-medium text-gray-800 dark:text-white">
-              Profile tab's associated content
-            </strong>
-            . Clicking another tab will toggle the visibility of this one for
-            the next. The tab JavaScript swaps classes to control the content
-            visibility and styling.
-          </p>
-        </div>
-        <div
-          className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-          id="dashboard"
-          role="tabpanel"
-          aria-labelledby="dashboard-tab"
-        >
-          <p className="text-sm text-gray-500 dark:text-gray-400">
-            This is some placeholder content the{" "}
-            <strong className="font-medium text-gray-800 dark:text-white">
-              Dashboard tab's associated content
-            </strong>
-            . Clicking another tab will toggle the visibility of this one for
-            the next. The tab JavaScript swaps classes to control the content
-            visibility and styling.
-          </p>
-        </div>
-        <div
-          className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-          id="settings"
-          role="tabpanel"
-          aria-labelledby="settings-tab"
-        >
-          <p className="text-sm text-gray-500 dark:text-gray-400">
-            This is some placeholder content the{" "}
-            <strong className="font-medium text-gray-800 dark:text-white">
-              Settings tab's associated content
-            </strong>
-            . Clicking another tab will toggle the visibility of this one for
-            the next. The tab JavaScript swaps classes to control the content
-            visibility and styling.
-          </p>
-        </div>
-        <div
-          className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-          id="contacts"
-          role="tabpanel"
-          aria-labelledby="contacts-tab"
-        >
-          <p className="text-sm text-gray-500 dark:text-gray-400">
-            This is some placeholder content the{" "}
-            <strong className="font-medium text-gray-800 dark:text-white">
-              Contacts tab's associated content
-            </strong>
-            . Clicking another tab will toggle the visibility of this one for
-            the next. The tab JavaScript swaps classes to control the content
-            visibility and styling.
-          </p>
-        </div>
+
+      <div className="text-black dark:text-white">
+        {tabs.map((tab) => (
+          <div
+            className={`p-4 rounded-lg bg-gray-50 dark:bg-gray-800 ${
+              activeTab === tab.target ? "block " : "hidden"
+            }`}
+            id={tab.target.slice(1)}
+            role="tabpanel"
+            aria-labelledby={`${tab.target.slice(1)}-tab`}
+            key={tab.target}
+          >
+            {tab.content}
+          </div>
+        ))}
       </div>
     </>
   );
