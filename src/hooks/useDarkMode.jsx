d8a8695029f746ddddfaca8c6774ba30a0e7097e
ginoongflores <florespaul161@gmail.com>
2024-05-04 13:38:00 +0800

    fix(useDarkMode): restructure dark mode to prevent losing the state value of light or dark when page refresh

--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -1,20 +1,15 @@
 import { useEffect, useState } from "react";
 
 export const useDarkMode = () => {
-  const [theme, setTheme] = useState(localStorage.theme);
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
   const colorTheme = theme === "dark" ? "light" : "dark";
-  localStorage.setItem("theme", colorTheme);
 
   useEffect(() => {
     const root = window.document.documentElement;
     root.classList.remove(colorTheme);
     root.classList.add(theme);
 
-    if (localStorage.theme === "dark") {
-      localStorage.theme = "light";
-    } else {
-      localStorage.setItem("theme", "dark");
-    }
+    localStorage.setItem("theme", theme);
   }, [theme, colorTheme]);
 
   return [colorTheme, setTheme];
