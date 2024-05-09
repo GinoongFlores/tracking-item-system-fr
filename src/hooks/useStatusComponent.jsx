b5e0865670c62b6709f4955f13319a699b4d3cdb
Christian Paul Flores <florespaul161@gmail.com>
2024-05-09 22:18:51 +0800

    ref(useStatusComponent): color scheme improve for transfer item status

--- src/hooks/useStatusComponent.jsx
+++ src/hooks/useStatusComponent.jsx
@@ -1,7 +1,8 @@
 export const useStatusComponent = () => {
   return {
-    pending: "bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-300",
-    delivered:
+    delivered: "bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-300",
+    pending: "bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300",
+    approved:
       "bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300",
     received:
       "bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300",
